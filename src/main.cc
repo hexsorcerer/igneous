f2c58878e8627c7a311ad26f3d07c289a7ec5746
hexsorcerer <hexsorcerer03e9@gmail.com>
2020-04-16 20:24:58 -0400

    Setup project
    Added a simple executable along with cmake files that generate build files successfully.

---
+++ src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// just a little something simple while I setup the project
+int main(int argc, char** argv) {
+  std::cout << "Press q to exit" << std::endl;
+  while (getchar() != 'q') { }
+  
+  return 0;
+}
